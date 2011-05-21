b10122d535d8857bd20ebc81f7fbad262fd373b7
Fraser Tweedale <frase@frase.id.au>
2011-05-21 21:09:01 +1000

    setup.py - prepare ledgertools for the journey to PyPI

--- setup.py
+++ setup.py
@@ -1,12 +1,30 @@
 from distutils.core import setup
 
+with open('README') as file:
+    long_description = file.read()
+
 setup(
     name='ledgertools',
-    version='0.1dev',
-    description='Utilities for the Ledger accounting system',
+    version='0.1',
+    description='Ledger accounting system utilities',
     author='Fraser Tweedale',
     author_email='frase@frase.id.au',
     url='http://frase.id.au/repo/ledgertools.git',
     packages=['ltlib', 'ltlib.readers', 'ltlib.test'],
     scripts=['bin/lt-stmtproc', 'bin/lt-transact'],
+    data_files=[
+        ('doc/ledgertools', ['doc/.ltconfig.sample']),
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Financial and Insurance Industry',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Office/Business :: Financial :: Accounting',
+    ],
+    long_description=long_description,
 )
